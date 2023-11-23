ab9da5a9680d88369852b61ef3d58249471529d5
jwlee <first202301@gmail.com>
2023-11-23 13:21:20 +0900

    update fastapi server

--- pro_aug/main.py
+++ pro_aug/main.py
@@ -1,4 +1,4 @@
-# uvicorn main:app --reload --port 8009
+# fastapi 서버 실행 명령어: uvicorn main:app --reload --port 8009 (port 번호 변경 시, gene.py에 있는 port 번호도 함께 수정)
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 import pandas as pd
